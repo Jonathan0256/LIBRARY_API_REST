e48eb891a76e6620393cac079c5038a8f6228208
Jonathan <jrodas753@boscdelacoma.cat>
2024-11-06 13:28:23 +0100

    commit-5 | fet endpoint de register + millora general codi

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,12 +3,66 @@ import fs from "fs";
 import crypto from "crypto";
 import dotenv from "dotenv";
 
+dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
+
+//Functions
+const saveUsers = (file, users) => {
+  fs.writeFileSync(file, JSON.stringify(users));
+};
+
+const loadUsers = (file) => {
+  if (fs.existsSync(file)) {
+    try {
+      const fileContent = fs.readFileSync(file, "utf8");
+      const parsedFile = JSON.parse(fileContent);
+      return Array.isArray(parsedFile) ? parsedFile : [];
+    } catch (error) {
+      console.error("Error al carregar els usuaris: ", error);
+      return [];
+    }
+  }
+  return [];
+};
+
 if (!SECRET_KEY) {
   console.error("La variable d'entorn no esta definida");
   process.exit(1);
 }
 
-export const login = (req, res) => {
+export const register = (req, res) => {
   const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Usuari i contraseña son obligatoris" });
+  }
+
+  const usersFile = "./models/users.json";
+  const users = loadUsers(usersFile);
+
+  if (!Array.isArray(users)) {
+    console.error("Users no es un array:", users);
+    return res.status(500).json({ error: "Error intern del servidor" });
+  }
+
+  const existingUser = users.find((c) => c.username === username);
+
+  if (existingUser) {
+    return res.status(409).json({ error: "L'usuari ja existeix" });
+  }
+
+  const passwordEncryption = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+
+  const newUser = { username, password: passwordEncryption };
+
+  users.push(newUser);
+
+  saveUsers(usersFile, users);
+
+  res.status(201).json({ message: "Usuari registrart correctament" });
 };
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/routes.js";
 dotenv.config();
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 
-app.get("/login", (req, res) => {
-  res.send("hey");
-});
+app.use("/api/v1/auth", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionant en http://localhost:${PORT}`);
