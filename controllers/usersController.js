e213d06a11f7bad41db99af6494f016bf691b3ed
Jonathan <jrodas753@boscdelacoma.cat>
2024-11-06 01:16:41 +0100

    commit-4 | fase 2 modularització + coses varies

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import crypto from "crypto";
 import dotenv from "dotenv";
 
+const SECRET_KEY = process.env.SECRET_KEY;
 if (!SECRET_KEY) {
   console.error("La variable d'entorn no esta definida");
   process.exit(1);
@@ -10,7 +11,4 @@ if (!SECRET_KEY) {
 
 export const login = (req, res) => {
   const { username, password } = req.body;
-
-  if (username === "hi" && password === "hey") {
-  }
 };
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import routes from "./routes/routes.js";
 
 dotenv.config();
 const app = express();
 const PORT = 3000;
-const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-app.use(express.json());
-
-app.post("/login", (req, res) => {});
+app.get("/login", (req, res) => {
+  res.send("hey");
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionant en http://localhost:${PORT}`);
